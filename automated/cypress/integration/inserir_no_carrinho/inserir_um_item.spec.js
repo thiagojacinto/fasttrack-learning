654de1fc0d26a51b118b7a9571bd345c25d2f5b5
thiagojacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-16 18:03:44 -0300

    feat: import dos pageObjects

--- automated/cypress/integration/inserir_no_carrinho/inserir_um_item.spec.js
+++ automated/cypress/integration/inserir_no_carrinho/inserir_um_item.spec.js
@@ -1,8 +1,26 @@
 /* global Given, When, And, Then */
+/// <reference types="Cypress" />
 
-Given(`esteja no home`, () => {});
-When(`inserir um item no carrinho`, () => {});
-Then(`deve exibí-lo no carrinho`, () => {});
+import HomePage from "../../support/pageObjects/Home/Home.page";
+import ProductView from "../../support/pageObjects/ProductView/ProductView.page";
 
-And(`inserir outro item`, () => {});
-Then(`deve exibí-los no carrinho`, () => {});
+Given(`esteja na home`, () => {
+    HomePage.acessarHome();
+});
+
+When(`inserir um item no carrinho`, () => {
+    HomePage.escolheProduto();
+    ProductView.adicionarAoCarrinho();
+});
+
+Then(`deve exibí-lo no carrinho`, () => {
+    return false;
+});
+
+And(`inserir outro item`, () => {
+    return false;
+});
+
+Then(`deve exibí-los no carrinho`, () => {
+    return false;
+});
