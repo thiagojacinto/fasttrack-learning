96ad0344c1dccdbafae9dd57ec7436ec57b9c246
Thiago Jacinto <thiagojs.santos@gmail.com>
2020-10-26 15:58:56 -0300

    fix: selecionar lista de produtos da Home

--- automated/cypress/support/pageObjects/Home/componentes.js
+++ automated/cypress/support/pageObjects/Home/componentes.js
@@ -2,5 +2,5 @@ export default {
     // botaoCarrinho: "div#header div.ctn-icones > a",
     // listaProdutos: "section#home a.ctn-produto",
     botaoCarrinho: ".topbar__extra-shopping_cart > a:nth-child(1)",
-    listaProdutos: "main.catalog div.container section.product__list div.container",
+    listaProdutos: "main.catalog div.container section.product__list div.container article a",
 };
