27ca16d24f3fc7931f20dc31ff31743e38630244
Thiago Jacinto <thiagojs.santos@gmail.com>
2020-10-26 15:59:41 -0300

    feat: botaoVoltar + ajustes nas opcoes

--- automated/cypress/support/pageObjects/ProductView/componentes.js
+++ automated/cypress/support/pageObjects/ProductView/componentes.js
@@ -1,4 +1,5 @@
 export default {
-    opcoes: "div.options__picker",
+    opcoes: "div.options__picker button.size__option",
     botaoAdicionar: "div.product section.cart__options button.add__to__cart",
+    botaoVoltar: "button.back__button"
 };
